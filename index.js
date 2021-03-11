6f542d90d69e0a932b0644b6045baad21243bf77
Dilshaad21 <dilshaadmuthalif21@gmail.com>
2021-03-11 08:56:29 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,75 +1,34 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const path = require('path');
+const generatePassword = require('password-generator');
+
 const app = express();
-const bodyParser = require("body-parser");
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Put all API endpoints under '/api'
+app.get('/api/passwords', (req, res) => {
+  const count = 5;
+
+  // Generate some passwords
+  const passwords = Array.from(Array(count).keys()).map(i =>
+    generatePassword(12, false)
+  )
+
+  // Return them as json
+  res.json(passwords);
+
+  console.log(`Sent ${count} passwords`);
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 const port = process.env.PORT || 5000;
-const cors = require("cors");
-const Card = require("./models/card");
-const path = require("path");
+app.listen(port);
 
-const MONGO_URI =
-  "mongodb+srv://Dilshaad:h80mGOG0WNFafYBN@cluster0.1xgor.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
-
-mongoose.connection.once("open", () => {
-  console.log("mongo db connected with the database");
-});
-
-app.use(express.static(path.join(__dirname, "client/build")));
-
-app.use(bodyParser.json());
-app.use(cors());
-
-app.get("/get-data", (req, res) => {
-  console.log("/ get request");
-
-  Card.find((err, card) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.send(card);
-    }
-  });
-});
-
-app.post("/post-data", (req, res) => {
-  console.log("/ post requested...");
-  let data = req.body.columns,
-    newData = [];
-  Card.remove({}, (err, cnt) => {
-    console.log(cnt);
-  });
-  console.log(data);
-  data.map((outer) => {
-    outer.cards.map((inner) => {
-      newData.push(
-        Card({
-          title: inner.title,
-          description: inner.description,
-          category: outer.title,
-        })
-      );
-      return null;
-    });
-    return null;
-  });
-
-  newData.forEach((item) => {
-    item.save();
-  });
-
-  res.send("success..");
-});
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
-});
-
-app.listen(port, "localhost", () => {
-  console.log("Server is running...", port);
-});
+console.log(`Password generator listening on ${port}`);
